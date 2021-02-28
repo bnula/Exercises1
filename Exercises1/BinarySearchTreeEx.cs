80c43313852c18547ba04129704663941e115983
bnula <bnulagnula@gmail.com>
2021-02-28 15:01:00 +0100

    created BinarySearchTree class

---
+++ Exercises1/BinarySearchTreeEx.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises1
+{
+    class BinarySearchTree
+    {
+        internal class Node
+        {
+            internal int value;
+            internal Node left;
+            internal Node right;
+        }
+
+        Node root = new Node();
+
+        public void Insert(int value)
+        {
+            Node addNode = new Node
+            {
+                value = value
+            };
+            if (root.value == 0)
+            {
+                root = addNode;
+                return;
+            };
+            Node current = root;
+            while (true)
+            {
+                if (value < current.value)
+                {
+                    if (current.left == null)
+                    {
+                        current.left = addNode;
+                        return;
+                    }
+                    current = current.left;
+                }
+                else
+                {
+                    if (current.right == null)
+                    {
+                        current.right = addNode;
+                        return;
+                    }
+                    current = current.right;
+                }
+            }
+        }
+
+        public void Remove(int value)
+        {
+            Node current = root;
+            Node prev = new Node();
+            while (true)
+            {
+                if (value < current.value)
+                {
+                    prev = current;
+                    current = current.left;
+                }
+                else if (value > current.value)
+                {
+                    prev = current;
+                    current = current.right;
+                }
+                else
+                {
+                    // leaf node
+                    if (current.left == null && current.right == null)
+                    {
+                        if (prev.value > current.value)
+                        {
+                            prev.left = null;
+                        }
+                        else
+                        {
+                            prev.right = null;
+                        }
+                        return;
+                    }
+                    // no right node
+                    else if (current.right == null)
+                    {
+                        if (prev.value > current.value)
+                        {
+                            prev.left = current;
+                        }
+                        else
+                        {
+                            prev.right = current;
+                        }
+                        return;
+                    }
+                    else
+                    {
+                        // if no left node in right node
+                        if (current.right.left == null)
+                        {
+                            current.right.left = current.left;
+                            if (prev.value > current.value)
+                            {
+                                prev.left = current.right;
+                            }
+                            else
+                            {
+                                prev.right = current.right;
+                            }
+                            return;
+                        }
+                        // take left node of the right node and replace the current node
+                        else
+                        {
+                            current.right.left.left = current.left;
+                            current.right.left.right = current.right;
+                            if (prev.value > current.value)
+                            {
+                                prev.left = current.right.left;
+                            }
+                            else
+                            {
+                                prev.right = current.right.left;
+                            }
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public Node Find(int value)
+        {
+            Node current = root;
+            while (current != null)
+            {
+                if (value < current.value)
+                {
+                    current = current.left;
+                }
+                else if (value > current.value)
+                {
+                    current = current.right;
+                }
+                else
+                {
+                    return current;
+                }
+            }
+            Console.WriteLine("Not Found");
+            return current;
+        }
+    }
+}
--- Exercises1/Program.cs
+++ Exercises1/Program.cs
@@ -11,14 +11,23 @@ namespace Exercises1
     {
         static void Main(string[] args)
         {
-            var a = new QueueEx();
-            a.Enqueue("test");
-            Console.WriteLine(a.Peek());
-            a.Enqueue(2);
-            a.Enqueue(3);
-            Console.WriteLine(a.Dequeue());
-            Console.WriteLine(a.Dequeue());
-            Console.WriteLine(a.Dequeue());
+            var a = new BinarySearchTree();
+            var res = a.Find(25);
+            Console.WriteLine(res);
+            a.Insert(50);
+            a.Insert(40);
+            a.Insert(30);
+            a.Insert(20);
+            a.Insert(10);
+            a.Insert(60);
+            a.Insert(70);
+            a.Insert(80);
+            a.Insert(90);
+            a.Insert(33);
+            res = a.Find(30);
+            Console.WriteLine(res.value);
+            Console.WriteLine(res.left.value);
+            Console.WriteLine(res.right.value);
         }
 
     }
